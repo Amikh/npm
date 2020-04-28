b011c6646968c6768db71d6455ace09e18807d5e
AM <amikh05@gmail.com>
2020-04-28 11:08:38 +0300

    fix and add ReadME

---
+++ README.md
@@ -0,0 +1,33 @@
+
+# 
+
+Send email - nodeJS Samples.
+
+## Installation
+
+Clone with HTTPS or SSH. 
+
+
+```bash
+git clone https://github.com/Amikh/npm.git
+```
+
+## Usage
+
+```bash
+Sign up to mailtrap  https://mailtrap.io/ 
+get user name and password 
+npm install nodemailer
+node sendmail.js
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+<!-- ## License
+[MIT](https://choosealicense.com/licenses/mit/) -->
+<!-- <p align="center">
+  <img src="" width="100%" height="15%">
+</p> -->
--- sendmail.js
+++ sendmail.js
@@ -4,16 +4,16 @@ let transport = nodemailer.createTransport({
     host: 'smtp.mailtrap.io',
     port: 2525,
     auth: {
-       user: '1178f646e80af1',
-       pass: '6a0172eecd9ab2'
+       user: '**************',
+       pass: '**************',
     }
 });
 
 const message = {
-    from: 'elonmusk@tesla.com',
+    from: 'santa@gmail.com',
     to: 'to@email.com',
-    subject: 'Design Your Model S | Tesla',
-    html: '<h1>Have the most fun you can in a car!</h1><p>Get your <b>Tesla</b> today!</p>',
+    subject: 'HO HO HO',
+    html: '<h1>Happy new year!</p>',
     attachments: [
         { // Use a URL as an attachment
           filename: 'index.html',
